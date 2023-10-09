e5d1038e7576520c29550dd2bbb961463f5fb978
roshanaale9818 <itachiuchhia9818@gmail.com>
2023-10-09 11:54:10 +1100

    downoad issue fixed for resume download

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -4,33 +4,31 @@ const AboutMe = (props)=>{
     const [imgObj,setImgObj] = useState({});
     const onDownloadHandler = async()=>{
         try{
-        //  const response = await  _axiosInstance.get('resume/download');
-        //    const filename = response.headers.get('Content-Disposition').split('filename=')[1];
-        
-        //    // Get the content type from the Content-Type header
-        //    const content_type = response.headers.get('Content-Type');
-   
-        //    // Create a blob from the response data
-        //    const blob = await response.blob();
-   
-        //    // Create a URL for the blob
-        //    const url = window.URL.createObjectURL(blob);
-   
-        //    // Create a temporary anchor element
-        //    const link = document.createElement('a');
-        //    link.href = url;
-        //    link.setAttribute('download', filename);
-   
-        //    // Append the anchor element to the body and trigger a click event
-        //    document.body.appendChild(link);
-        //    link.click();
-   
-        //    // Clean up the URL object after the download
-        //    window.URL.revokeObjectURL(url);
+     
+            // Make a GET request to the backend endpoint
+            const response = await _axiosInstance.get('resume/download', {
+              responseType: 'blob', // Important: responseType must be 'blob' for binary data
+            });
+      
+            // Create a URL for the blob
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+      
+            // Create a temporary anchor element
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'myresume-file.pdf');
+      
+            // Append the anchor element to the body and trigger a click event
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+      
+            // Clean up the URL object after the download
+            window.URL.revokeObjectURL(url);
 
         }
         catch(err){
-
+            console.err("Error:",err);
         }
     }
     useEffect(()=>{
@@ -75,7 +73,7 @@ return <React.Fragment>
                                     <img className="aboutImage" src={imgObj.image_url}alt="fig" />
                                 </div>
                                 <div className="download-wrap txt-center">
-                                    <a href="download/roshanaalemagar" onClick={onDownloadHandler} className="download-btn" download="roshanaalemagar" target="_blank">DOWNLOAD RESUME <i className="fa fa-download ml-2"></i></a>
+                                    <button onClick={onDownloadHandler} className="download-btn" download="roshanaalemagar" target="_blank">DOWNLOAD RESUME <i className="fa fa-download ml-2"></i></button>
                                 </div>
                             </div>
                             <div className="col-lg-8 col-12">
